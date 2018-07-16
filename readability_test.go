be47a99698ec724c075a40add74b6fd62d6f3f20
Naiba <1@5.nu>
2018-07-16 16:35:53 +0800

    follow upstream @7cf95bd

--- readability_test.go
+++ readability_test.go
@@ -14,8 +14,8 @@ import (
 func TestParse(t *testing.T) {
 	pageUrls := []string{
 		"https://www.jianshu.com/p/725c7dc55d58",
-		"https://www.geekonomics10000.com/973",
 		"http://politics.people.com.cn/n1/2018/0612/c1001-30051069.html",
+		"http://www.xinhuanet.com/politics/2018-07/16/c_1123129685.htm",
 	}
 	for page := 0; page < len(pageUrls); page++ {
 		resp, err := http.Get(pageUrls[page])
@@ -26,13 +26,11 @@ func TestParse(t *testing.T) {
 		if err != nil {
 			continue
 		}
-		article, err := New(Option{Debug: true, PageURL: pageUrls[page]}).Parse(string(body))
+		article, err := New(Option{Debug: false, PageURL: pageUrls[page]}).Parse(string(body))
 		if err != nil {
 			panic(err)
 		} else {
 			t.Log("标题", article.Title)
-			//t.Log("摘要", article.Excerpt)
-			//t.Log("正文", article.Content)
 		}
 		resp.Body.Close()
 	}
